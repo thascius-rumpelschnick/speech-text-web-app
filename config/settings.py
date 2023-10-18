15648f218a60e0b72df89e5571ba5ce859c691cb
Florian Zapf <zapforama@googlemail.com>
2023-10-18 14:25:07 +0200

    Update env file and let settings set by it

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environment['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environment['DEBUG']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [environment['ALLOWED_HOSTS']]
 
 # Application definition
 
