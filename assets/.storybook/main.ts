0fdcfb5cb9dc05d8c2f6f11bd252d15a89693086
Florian Zapf <zapforama@googlemail.com>
2023-10-29 00:44:17 +0200

    Update storybook

--- assets/.storybook/main.ts
+++ assets/.storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
         "@storybook/addon-onboarding",
         "@storybook/addon-interactions",
         "@storybook/preset-scss",
+        "@storybook/addon-mdx-gfm"
     ],
     framework: {
         name: "@storybook/react-webpack5",
