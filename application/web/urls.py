42755ecc9cd9b9b717b9fdbaf2ecb5062a352d3f
Florian Zapf <zapforama@googlemail.com>
2023-11-11 17:09:52 +0100

    [speech-text-web-app] Basic Frontend Scaffolding
    - Use class based view

--- application/web/urls.py
+++ application/web/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from application.web.views import IndexView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', IndexView.as_view(), name='index'),
 ]
--- application/web/views.py
+++ application/web/views.py
@@ -1,12 +1,15 @@
+from django.views import View
 from django.shortcuts import render
 
 
-def index(request):
-    user = {'id': 1, 'name': 'User_1'}
-    model = {'title': 'Django with Webpack and Babel'}
+class IndexView(View):
 
-    context = {
-        'element_id': 'index',
-        'view_model': {'user': user, 'model': model}
-    }
-    return render(request, 'page.html', context)
+    def get(self, request):
+        user = {'id': 1, 'name': 'User_1'}
+        model = {'title': 'Django with Webpack and Babel'}
+
+        context = {
+            'element_id': 'index',
+            'view_model': {'user': user, 'model': model}
+        }
+        return render(request, 'page.html', context)
