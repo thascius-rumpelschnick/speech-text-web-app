61aa4f54dab127fed4972f03bbb0938d20eaac18
Florian Zapf <zapforama@googlemail.com>
2024-03-11 14:30:35 +0100

    [speech-text-web-app] Deployment
    - Adjust wording
    - Fix storybook compile error
    - Clean-up

--- assets/tsx/components/editor/Editor.stories.tsx
+++ assets/tsx/components/editor/Editor.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { StorybookContextProvider } from "../../hooks/AppContext";
+import { Transcription } from "../../interfaces/ContainerProps";
 import Editor from "./Editor";
 
 const meta = {
@@ -8,23 +9,23 @@ const meta = {
     component: Editor,
     decorators: [
         (Story, context) => (
-            <div style={{ minHeight: "500px" }}>
-                <StorybookContextProvider storybookContext={context.args}>
+            <div style={ { minHeight: "500px" } }>
+                <StorybookContextProvider storybookContext={ context.args }>
                     <Story />
                 </StorybookContextProvider>
             </div>
-        ),
+        )
     ],
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
         layout: "fullscreen",
         backgrounds: {
             default: "darkish",
-            values: [ { name: "darkish", value: "rgb(33, 37, 41)" } ],
-        },
+            values: [ { name: "darkish", value: "rgb(33, 37, 41)" } ]
+        }
     },
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-    tags: [ "autodocs" ],
+    tags: [ "autodocs" ]
 } satisfies Meta<typeof Editor>;
 
 export default meta;
@@ -32,7 +33,13 @@ type Story = StoryObj<typeof meta>;
 
 export const TextEditor: Story = {
     args: {
-        user: { id: 1, name: "Foo" },
-        model: { title: "Hello World" },
-    },
+        transcription: {
+            id: 1,
+            content: "This is a test transcription",
+            createdAt: "2021-10-10T10:10:10",
+            updatedAt: "2021-10-10T10:10:10",
+        },
+        updateTranscription: (transcription: Transcription) => alert(`Updated transcription: ${transcription.content}`),
+        isDebug: true,
+    }
 };
--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('application.web.urls')),
-    # path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
